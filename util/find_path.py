c2f81fd6336c41c4a762d4c64e6a31c9ed3582aa
Jacob Cornwell <jacob.p.cornwell@gmail.com>
2017-11-06 19:21:45 -0600

    Clean up the find_path script a little

--- util/find_path.py
+++ util/find_path.py
@@ -1,50 +1,51 @@
 #!/usr/bin/python3
 
-# representation of node
-# (operation, operand, next_node)
+# nodes are represented as a list of possible next steps
+# steps are represented as (operation, operand, next_node)
 
-a = [[('+', 4, 4), ('+', 4, 2), ('-', 4, 2), ('-', 9, 1)],     # node 0
-     [('-', 4, 2), ('-', 11, 5), ('-', 18, 3), ('*', 18, 3)],  # node 1
-     [('+', 4, 4), ('*', 4, 4), ('*', 8, 6), ('*', 11, 5), 
-      ('-', 11, 5), ('-', 18, 3), ('-', 9, 1)],                # node 2
-     [('-', 9, 1), ('-', 11, 5), ('-', 4, 2), ('*', 11, 5), 
-      ('*', 1, 7), ('*', 9, 1)],                               # node 3
-     [('*', 8, 6), ('*', 11, 5), ('*', 4, 2), ('+', 4, 2)],    # node 4
-     [('*', 4, 2), ('*', 4, 4), ('*', 8, 6), ('-', 8, 6), 
-      ('-', 1, 7), ('*', 1, 7), ('*', 18, 3), ('-', 4, 2),
-      ('-', 9, 1), ('-', 18, 3)],                              # node 5
-     [('*', 4, 4), ('-', 1, 7), ('-', 11, 5), ('*', 11, 5), 
-      ('*', 4, 2), ('*', 4, 4)],                               # node 6
-     []]                                                       # node 7
+nodes = [[('+', 4, 4), ('+', 4, 2), ('-', 4, 2), ('-', 9, 1)],     # node 0
+         [('-', 4, 2), ('-', 11, 5), ('-', 18, 3), ('*', 18, 3)],  # node 1
+         [('+', 4, 4), ('*', 4, 4), ('*', 8, 6), ('*', 11, 5), 
+          ('-', 11, 5), ('-', 18, 3), ('-', 9, 1)],                # node 2
+         [('-', 9, 1), ('-', 11, 5), ('-', 4, 2), ('*', 11, 5), 
+          ('*', 1, 7), ('*', 9, 1)],                               # node 3
+         [('*', 8, 6), ('*', 11, 5), ('*', 4, 2), ('+', 4, 2)],    # node 4
+         [('*', 4, 2), ('*', 4, 4), ('*', 8, 6), ('-', 8, 6), 
+          ('-', 1, 7), ('*', 1, 7), ('*', 18, 3), ('-', 4, 2),
+          ('-', 9, 1), ('-', 18, 3)],                              # node 5
+         [('*', 4, 4), ('-', 1, 7), ('-', 11, 5), ('*', 11, 5), 
+          ('*', 4, 2), ('*', 4, 4)],                               # node 6
+         []]                                                       # node 7
 
-stack = []
+path = []
 
-def f(node, val, length):
-  stack.append((node, val, length))
-  if len(a[node]) == 0 and val == 30:
+def find_path(cur_node, val, length):
+  path.append((cur_node, val, length))
+  if len(nodes[cur_node]) == 0 and val == 30:
     print("Success")
-    print(stack)
+    print(path)
+    exit()
   if length == 0:
-    stack.pop()
+    path.pop()
     return
   if val <= 0:
-    stack.pop()
+    path.pop()
     return
   if val > 1000000:
-    stack.pop()
+    path.pop()
     return
-  for edge in a[node]:
-    if edge[0] == '+':
-      val2 = val + edge[1]
-      f(edge[2], val2, length - 1)
-    if edge[0] == '-':
-      val2 = val - edge[1]
-      f(edge[2], val2, length - 1)
-    if edge[0] == '*':
-      val2 = val * edge[1]
-      f(edge[2], val2, length - 1)
-  stack.pop()
+  for edge in nodes[cur_node]:
+    operator, operand, next_node = edge
+    if operator == '+':
+      new_val = val + operand
+      find_path(next_node, new_val, length - 1)
+    if operator == '-':
+      new_val = val - operand
+      find_path(next_node, new_val, length - 1)
+    if operator == '*':
+      new_val = val * operand
+      find_path(next_node, new_val, length - 1)
+  path.pop()
 
-f(0, 22, 6)
-
-print('Done')
+for i in range(10):
+  find_path(0, 22, i)
